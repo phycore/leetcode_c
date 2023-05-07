6d49854bff69d35c518cd4d9405f2fc7a0c602cb
Jun-Yu Chen <phycore@gmial.com>
2023-05-07 23:05:46 +0800

    [logging]Import a simple logging module.

---
+++ logging/inc/logging.h
@@ -0,0 +1,13 @@
+#ifndef _LOGGING_H
+#define _LOGGING_H
+
+#include <stdio.h>
+
+// clang-format off
+#define log_debug(...) printf(__VA_ARGS__); printf("\n");
+#define log_info(...) printf(__VA_ARGS__); printf("\n");
+#define log_warn(...) printf(__VA_ARGS__); printf("\n");
+#define log_error(...) printf(__VA_ARGS__); printf("\n");
+// clang-format on
+
+#endif // _LOGGING_H
