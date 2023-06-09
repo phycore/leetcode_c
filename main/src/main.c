8649fdd2a9ca1d609ea89d00f2908ffb74ce8d8d
Jun-Yu Chen <phycore@gmial.com>
2023-06-10 00:12:43 +0800

    [Test]Using set data structure.

--- main/src/main.c
+++ main/src/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "Set.h"
 #include "logging.h"
 
 int main(int argc, char* argv[]) {
@@ -16,6 +17,8 @@ int main(int argc, char* argv[]) {
     // char* s = "abcdef";
     // char* s = "dvdf";
     char* s = "ccdefdvdf";
+
+    retval = set_test(s);
 
     return retval;
 }
