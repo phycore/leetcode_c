7f8b4fe8a45121f819fa5a1c8e2c7a95dde83299
Jun-Yu Chen <phycore@gmial.com>
2024-12-06 16:51:00 +0800

    [Singly List]Add operations for singly list.

--- linked_list/inc/Linked_List.h
+++ linked_list/inc/Linked_List.h
@@ -1,11 +1,27 @@
 #ifndef _LINKED_LIST_H
 #define _LINKED_LIST_H
 
+#include <stddef.h>
+
 // Definition for singly-linked list.
 struct ListNode {
     int val;
     struct ListNode* next;
 };
+
+struct SinglyList {
+    struct ListNode* p_Head;
+    size_t length;
+};
+
+struct SinglyList* NewList(void);
+void DeleteList(struct SinglyList* p_List);
+
+void PushNode(struct SinglyList* p_List, int push_val);
+void DeleteNode(struct SinglyList* p_List, int del_val);
+
+void TraversePrintNodes(struct SinglyList* p_List);
+size_t GetListLength(struct SinglyList* p_List);
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2);
 #endif
--- linked_list/src/Linked_List.c
+++ linked_list/src/Linked_List.c
@@ -6,6 +6,104 @@
 
 #include "log.h"
 
+static struct ListNode* NewNode(int set_val) {
+    struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    if (NULL == node) {
+        return NULL;
+    }
+    node->next = NULL;
+    node->val = set_val;
+
+    return node;
+}
+
+struct SinglyList* NewList(void) {
+    struct SinglyList* p_self = (struct SinglyList*)malloc(sizeof(struct SinglyList));
+    if (NULL == p_self) {
+        return NULL;
+    }
+    p_self->p_Head = NULL;
+    p_self->length = 0;
+
+    return p_self;
+}
+
+void DeleteList(struct SinglyList* p_List) {
+    struct ListNode* current = NULL;
+    struct ListNode* next = NULL;
+    current = p_List->p_Head;
+    while (NULL != current) {
+        next = current->next;
+        free(current);
+        current = next;
+        p_List->length--;
+    }
+    free(p_List);
+    p_List = NULL;
+}
+
+void PushNode(struct SinglyList* p_List, int push_val) {
+    struct ListNode* head_node = p_List->p_Head;
+    struct ListNode* current_node = head_node;
+    if (NULL == head_node) {
+        p_List->p_Head = NewNode(push_val);
+    } else {
+        // current_node = head_node->next;
+        while (NULL != current_node->next) {
+            current_node = current_node->next;
+        }
+        current_node->next = NewNode(push_val);
+    }
+    p_List->length++;
+}
+
+void DeleteNode(struct SinglyList* p_List, int del_val) {
+    struct ListNode* previous = p_List->p_Head;
+    struct ListNode* current = p_List->p_Head;
+
+    while (NULL != current) {
+        if (del_val == current->val) {
+            // 1st node
+            if (current == previous) {
+                p_List->p_Head = current->next;
+                free(p_List->p_Head);
+                p_List->length--;
+            } else {
+                previous->next = current->next;
+                free(current);
+                p_List->length--;
+            }
+        }
+        previous = current;
+        current = current->next;
+    }
+}
+
+void TraversePrintNodes(struct SinglyList* p_List) {
+    if (NULL != p_List) {
+        size_t idx = 0;
+        struct ListNode* current = p_List->p_Head;
+        while (NULL != current) {
+            log_info("%s, node[%d] = %d", __func__, idx, current->val);
+            current = current->next;
+            idx++;
+        }
+    }
+}
+
+size_t GetListLength(struct SinglyList* p_List) {
+    size_t length = 0;
+    if (NULL != p_List) {
+        struct ListNode* current = p_List->p_Head;
+        while (NULL != current) {
+            current = current->next;
+            length++;
+        }
+    }
+
+    return length;
+}
+
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     size_t node_size = sizeof(struct ListNode);
     struct ListNode* p_Node1 = l1;
